52720c9896e66d62b1b8a31929681c1f0a4e33ca
lizyangcoco <1005481165@qq.com>
2020-04-14 22:07:03 +0800

    线程<守护,优先级,让出,加入>

---
+++ src/main/java/com/example/demo/Javax0基础/JavaX基础语法/线程/ThreadPriority.java
@@ -0,0 +1,35 @@
+package com.example.demo.Javax0基础.JavaX基础语法.线程;
+
+/**
+ * 线程使用方法
+ * Created by 李泽阳 on 2020/4/14 21:30
+ */
+public class ThreadPriority implements Runnable {
+
+    @Override
+    public void run() {
+        //让出线程，cpu在执行时候比较均匀一些，执行完当前任务后，让出cpu
+        Thread.yield();
+        System.out.println("线程优先级");
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new ThreadPriority());
+        Thread t2 = new Thread(new ThreadPriority());
+        //
+        t2.setPriority(Thread.MAX_PRIORITY);//线程的优先级(优先级10-1，平均为5)
+        t1.start();
+        try {
+            //加入线程必须在，先执行的线程start下面
+            t1.join();//加入线程
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //设置子线程为守护线程，当主线程执行完毕后，子线程进行关闭
+        t2.setDaemon(true);
+        t2.start();
+
+        System.out.println("主线程结束");
+
+    }
+}
